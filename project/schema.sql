27403327d5fb6112303219bd6737e170f1bdcafd
mp40 <philps.matthew@gmail.com>
2022-11-13 14:59:50 +0100

    test connects to db

--- project/schema.sql
+++ project/schema.sql
@@ -3,9 +3,9 @@ drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
   name text not null,
-  brewery text not null
-  abv float not null
-  type text not null
-  receptacle_size integer not null
+  brewery text not null,
+  abv float not null,
+  type text not null,
+  receptacle_size integer not null,
   receptacle_type text not null
 );
--- tests/app_test.py
+++ tests/app_test.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from project.app import app
 
 
@@ -7,3 +9,7 @@ def test_index():
 
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
+
+def test_database():
+    assert Path("crafty_danes.db").is_file()
