d2f947f61372f7d4072cbfe663c3d55f70fec230
mp40 <philps.matthew@gmail.com>
2022-11-16 22:06:21 +0100

    add get beers test

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from unittest import mock
+import json
 
 from project.app import app, init_db
+from seed_beers import seed_beers
 
 @mock.patch("project.app.hello", return_value="Hello, World!", autoSpec=True)
 def test_index(mock_hello):
@@ -11,7 +13,15 @@ def test_index(mock_hello):
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
 
+@mock.patch("project.app.get_beers", return_value=json.dumps(seed_beers[0]), autoSpec=True)
+def test_beers(mock_get_beers):
+    request = app.test_client()
+    response = request.get("/beers")
+
+    assert response.status_code == 200
+    assert response.data == bytes(json.dumps(seed_beers[0]), encoding='utf8')
+
 
 def test_database():
     init_db()
-    assert Path("crafty_danes.db").is_file()
+    assert Path("crafty_danes.db").is_file()    
