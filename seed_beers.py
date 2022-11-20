41cce930c6c8dba84cc6803e4a1d20a5f8486ea5
mp40 <philps.matthew@gmail.com>
2022-11-20 10:55:44 +0100

    more seed beers

---
+++ seed_beers.py
@@ -0,0 +1,56 @@
+seed_beers = [
+    {
+        "id": 1,
+        "name": "Pink Farts & Unicorns",
+        "brewery":"Amager Bryghus",
+        "abv": 4.6,
+        "type": "IPL",
+        "receptacle_size": 500,
+        "receptacle_type": "can"
+    },
+    {
+        "id": 2,
+        "name": "Lardy-Dardy",
+        "brewery":"Amager Bryghus",
+        "abv": 4.5,
+        "type": "Hazy Session IPA",
+        "receptacle_size": 500,
+        "receptacle_type": "can"
+    },
+    {
+        "id": 3,
+        "name": "Magic Dragon",
+        "brewery":"Svaneke",
+        "abv": 5.0,
+        "type": "Lager",
+        "receptacle_size": 330,
+        "receptacle_type": "can"
+    },
+        {
+        "id": 4,
+        "name": "Sweet Mary",
+        "brewery":"Svaneke",
+        "abv": 7.9,
+        "type": "Dobbelbock",
+        "receptacle_size": 500,
+        "receptacle_type": "bottle"
+    },
+    {
+        "id": 5,
+        "name": "Evergreen",
+        "brewery":"Mikkeller",
+        "abv": 3.5,
+        "type": "Hazy IPA",
+        "receptacle_size": 330,
+        "receptacle_type": "can"
+    },
+    {
+        "id": 6,
+        "name": "Burst",
+        "brewery":"Mikkeller",
+        "abv": 5.5,
+        "type": "IPA",
+        "receptacle_size": 330,
+        "receptacle_type": "can"
+    }
+]
