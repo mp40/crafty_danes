81b0768e17ab82d866c41b48e3fb65e58bbae427
mp40 <philps.matthew@gmail.com>
2022-11-20 10:55:21 +0100

    fix test for getting all beers

--- tests/app_test.py
+++ tests/app_test.py
@@ -13,13 +13,13 @@ def test_index(mock_hello):
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
 
-@mock.patch("project.app.get_beers", return_value=json.dumps(seed_beers[0]), autoSpec=True)
+@mock.patch("project.app.get_beers", return_value=json.dumps(seed_beers), autoSpec=True)
 def test_beers(mock_get_beers):
     request = app.test_client()
     response = request.get("/beers")
 
     assert response.status_code == 200
-    assert response.data == bytes(json.dumps(seed_beers[0]), encoding='utf8')
+    assert response.data == bytes(json.dumps(seed_beers), encoding='utf8')
 
 
 def test_database():
