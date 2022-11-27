f40088cab304a534c84adc9cb211031b29e872a7
mp40 <philps.matthew@gmail.com>
2022-11-27 08:43:03 +0100

    add failing test

--- tests/app_test.py
+++ tests/app_test.py
@@ -11,9 +11,17 @@ def test_index():
     assert response.data == b"Hello, Beers!"
 
 @mock.patch("project.app.get_beers", return_value=seed_beers, autoSpec=True)
-def test_beers(mock_get_beers):
+def test_get_beers(mock_get_beers):
     request = app.test_client()
     response = request.get("/beers")
+
+    assert response.status_code == 200
+    assert response.json == seed_beers
+
+@mock.patch("project.app.get_beer_by_id", return_value=seed_beers[0], autoSpec=True)
+def test_get_beer_by_id(mock_get_beer):
+    request = app.test_client()
+    response = request.get("/beers/<id>")
 
     assert response.status_code == 200
     assert response.json == seed_beers
